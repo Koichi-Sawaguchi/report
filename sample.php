86e2ac11da0186f56466ef1be84860e60c45ac52
zekouhongyi@gmail.com <zekouhongyi@gmail.com>
2021-06-19 06:56:59 +0000

    second commit

--- sample.php
+++ sample.php
@@ -1,34 +1,40 @@
 <?php
 echo 'hello php!';
 
-$a = 3;
-$b = 7;
-echo $a + $b;
-$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
-echo $array_month[7];
-$hello = "Hello,";
 $name = "koichi";
-$world = "'s world!";
-echo  $hello . "$name" . "$world";
-$tech = 'tech';
-$tech .=' _boost';
-echo $tech;
-$calendar_2018 = [
-  "January" => "1月",
-  "February" => "2月",
-  "March" => "3月",
-  "April" => "4月",
-  "May" => "5月",
-  "June" => "6月",
-  "July" => "7月",
-  "August" => "8月",
-  "September" => "9月",
-  "October" => "10月",
-  "November" => "11月",
-  "December" => "12月"
-];
+switch ($name) {
+   case "koichi":
+     echo "「私は　あなたの名前　です」";
+     break;
+     default:
+       echo "「あなたの名前ではありません」";
+       break;
+}
+$total = 0;
+echo $total;
 
+for($i = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total;
 
-echo $calendar_2018["December"];
+$fruits =array ("apple","orange","banana","pair","peach");
+ 
+foreach($fruits as $fruits) {
+  echo  "" . $fruits;
+  echo "\n";
+  
+}
 
+$start = 1;
 
+$end = 101;
+  
+  for($i = $start; $i <= $end; $i++){
+    if($i % 5 ==0)
+    echo $i;
+  }  
+    
+  
+    
+  
